87608556658f13c1aa0210ff5d7c4964c5836d11
Sophia <1340126770@qq.com>
2023-12-11 16:13:35 -0500

    m

--- server_connection.py
+++ server_connection.py
@@ -98,8 +98,13 @@ def pre_process_message(server_socket,client_socket):
         return None
     operation_type, file_size = struct.unpack('!IQ', message_data[:12])
     file_path = message_data[12:].decode()  # assuming the rest of the message is the file path
-
     print(operation_type,file_size,file_path)
+    if operation_type == "0":
+        print("Before serialize the file size!!!!")
+        file_data = recvall(client_socket, file_size)
+        print("After serialize the file size!!!!")
+        print(file_data)
+    
 
     return ""
 
