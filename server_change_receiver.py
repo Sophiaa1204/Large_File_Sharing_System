a14ece721fb9e9f53269fe3cbd8f5e4095ebcc87
Sophia <1340126770@qq.com>
2023-12-11 17:16:22 -0500

    m

--- server_change_receiver.py
+++ server_change_receiver.py
@@ -8,8 +8,7 @@ file_status_dict = {}
 
 def update_file_status(file_path):
     current_time = time.time()
-    with server_connection.lock:
-        file_status_dict[file_path] = current_time
+    file_status_dict[file_path] = current_time
 
 def remove_file_status(file_path):
     with server_connection.lock:
@@ -18,7 +17,7 @@ def remove_file_status(file_path):
 def handle_receive_update(save_path, file_data, client_socket):
     result, timestamp = check_file_status(save_path)
     
-    if True:
+    if result:
         with open(save_path, 'wb') as file:
             file.write(file_data)
         print("File received completely.")
@@ -28,21 +27,25 @@ def handle_receive_update(save_path, file_data, client_socket):
             update_file_status(file_path)
         else:
             update_file_status(file_path)
-        message_type = "confirmation"
-        send_data = {
-            "result": "SUCCESS",
-            "file_path": file_path
-        }
-        server_connection.send_to_client(client_socket,message_type,send_data)
 
-    else:
-        # The only possibility for a failed change is that the file has been deleted
-        message_type = "confirmation"
-        send_data = {
-            "result": "FAIL",
-            "file_path": save_path
-        }
-        server_connection.send_to_client(client_socket,message_type,send_data)
+    print("Until the end of handle received update!!!")
+
+
+    #     message_type = "confirmation"
+    #     send_data = {
+    #         "result": "SUCCESS",
+    #         "file_path": file_path
+    #     }
+    #     server_connection.send_to_client(client_socket,message_type,send_data)
+
+    # else:
+    #     # The only possibility for a failed change is that the file has been deleted
+    #     message_type = "confirmation"
+    #     send_data = {
+    #         "result": "FAIL",
+    #         "file_path": save_path
+    #     }
+    #     server_connection.send_to_client(client_socket,message_type,send_data)
 
 def handle_receive_add(save_path, file_data, client_socket):
     handle_receive_update(save_path, file_data, client_socket)
